6fdcf01b434338578658e70a9d3910915e3661ff
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2022-12-13 15:48:28 -0700

    added classes to structure

---
+++ Application/src/main/java/com/kenzie/appserver/service/model/Application.java
@@ -0,0 +1,15 @@
+package com.kenzie.appserver.service.model;
+
+public class Application {
+
+    //Application Class:
+    //UUID applicationId *Partition key*
+    //Timestamp timeStamp (time that application was most recently edited)
+    //File Resume
+    //List<String> workHistory
+    //List<String> references
+    //Criteria jobCriteria
+
+    //constructor for all arguments
+    //getters, no setters
+}
