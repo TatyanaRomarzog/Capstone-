f6b0dcaaae75f4305645080e5a1ccd35867e71bf
TiffanyHunnicutt <tiffany.hunnicutt@mykenzie.snhu.edu>
2022-12-14 20:01:25 -0600

    Application controller

--- Application/src/main/java/com/kenzie/appserver/controller/model/ApplicationCreateRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/ApplicationCreateRequest.java
@@ -1,4 +1,25 @@
 package com.kenzie.appserver.controller.model;
 
+//TODO: finish this class
 public class ApplicationCreateRequest {
+    public Object getApplicationId() {
+    }
+
+    public Object getTimeStamp() {
+    }
+
+    public Object getResume() {
+    }
+
+    public Object getReference() {
+    }
+
+    public Object getWorkHistory() {
+    }
+
+    public Object getJobCriteria() {
+    }
+
+    public Object getCriteria() {
+    }
 }
