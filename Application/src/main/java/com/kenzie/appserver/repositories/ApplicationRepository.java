b66ea6e626fc07be821cb001afec9ad172ae2269
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2022-12-20 15:35:23 -0700

    added method to grab by GSI

--- Application/src/main/java/com/kenzie/appserver/repositories/ApplicationRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ApplicationRepository.java
@@ -5,6 +5,9 @@ import com.kenzie.appserver.repositories.model.ApplicationRecord;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @EnableScan
 public interface ApplicationRepository extends CrudRepository<ApplicationRecord, String> {
+    List<ApplicationRecord> findByUserId(String userId);
 }
