ecd72cc61494ef02eaadf0346c537ecef0e25b18
TatyanaLavrenov <tatyana.lavrenov@mykenzie.snhu.edu>
2022-12-28 20:30:45 -0800

    User class

--- Application/src/main/java/com/kenzie/appserver/service/model/User.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/User.java
@@ -2,15 +2,52 @@ package com.kenzie.appserver.service.model;
 
 public class User {
 
-    //User Class:
-    //String username
-    //String password
-    //String firstName
-    //String middleName
-    //String lastName
-    //String phoneNumber
-    //String primaryEmail
+    private final String username;
+    private final String password;
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String phoneNumber;
+    private final String primaryEmail;
 
     //constructor for all arguments
-    //getter methods, NO setters
+    public User(String username, String password, String firstName, String middleName, String lastName, String phoneNumber, String primaryEmail) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.primaryEmail = primaryEmail;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPrimaryEmail() {
+        return primaryEmail;
+    }
+
+
+//getter methods, NO setters
 }
