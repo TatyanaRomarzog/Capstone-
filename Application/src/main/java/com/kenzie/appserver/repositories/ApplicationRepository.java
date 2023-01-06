520d6de46770a8c22ab009e05cebdbf3f1775eb6
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2023-01-06 14:25:22 -0700

    added a space to application repository

--- Application/src/main/java/com/kenzie/appserver/repositories/ApplicationRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ApplicationRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 @EnableScan
 public interface ApplicationRepository extends CrudRepository<ApplicationRecord, String> {
+
     List<ApplicationRecord> findByUsername(String username);
 }
