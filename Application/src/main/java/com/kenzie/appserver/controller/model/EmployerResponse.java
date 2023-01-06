0313425e7ffecbb9e27dba1ee9b81822d59ec337
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2023-01-06 01:00:51 -0700

    controller for employer done

--- Application/src/main/java/com/kenzie/appserver/controller/model/EmployerResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/EmployerResponse.java
@@ -1,2 +1,85 @@
-package com.kenzie.appserver.controller.model;public class EmployerResponse {
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EmployerResponse {
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("middleName")
+    private String middleName;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @JsonProperty("companyName")
+    private String companyName;
+
+    @JsonProperty("companyPhoneNumber")
+    private String companyPhoneNumber;
+
+    @JsonProperty("companyEmail")
+    private String companyEmail;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyPhoneNumber() {
+        return companyPhoneNumber;
+    }
+
+    public void setCompanyPhoneNumber(String companyPhoneNumber) {
+        this.companyPhoneNumber = companyPhoneNumber;
+    }
+
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public void setCompanyEmail(String companyEmail) {
+        this.companyEmail = companyEmail;
+    }
 }
--- Application/src/main/java/com/kenzie/appserver/controller/model/EmployerUpdateRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/EmployerUpdateRequest.java
@@ -1,2 +1,103 @@
-package com.kenzie.appserver.controller.model;public class EmployerUpdateRequest {
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotEmpty;
+
+public class EmployerUpdateRequest {
+
+    @NotEmpty
+    @JsonProperty("oldPassword")
+    private String oldPassword;
+
+    @NotEmpty
+    @JsonProperty("newPassword")
+    private String newPassword;
+
+    @NotEmpty
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("middleName")
+    private String middleName;
+
+    @NotEmpty
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @NotEmpty
+    @JsonProperty("companyName")
+    private String companyName;
+
+    @NotEmpty
+    @JsonProperty("companyPhoneNumber")
+    private String companyPhoneNumber;
+
+    @NotEmpty
+    @JsonProperty("companyEmail")
+    private String companyEmail;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyPhoneNumber() {
+        return companyPhoneNumber;
+    }
+
+    public void setCompanyPhoneNumber(String companyPhoneNumber) {
+        this.companyPhoneNumber = companyPhoneNumber;
+    }
+
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public void setCompanyEmail(String companyEmail) {
+        this.companyEmail = companyEmail;
+    }
 }
