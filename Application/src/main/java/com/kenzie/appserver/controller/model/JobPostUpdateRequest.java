61d2a5ea745e36e9be43daa4e2bf09abb93fff1b
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2023-01-06 17:58:52 -0700

    finished requests and responses for job post

--- Application/src/main/java/com/kenzie/appserver/controller/model/JobPostUpdateRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/JobPostUpdateRequest.java
@@ -1,4 +1,80 @@
 package com.kenzie.appserver.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotEmpty;
+
 public class JobPostUpdateRequest {
+
+    @NotEmpty
+    @JsonProperty("positionTitle")
+    private String positionTitle;
+
+    @NotEmpty
+    @JsonProperty("companyName")
+    private String companyName;
+
+    @NotEmpty
+    @JsonProperty("location")
+    private String location;
+
+    @NotEmpty
+    @JsonProperty("proposedSalary")
+    private Integer proposedSalary;
+
+    @NotEmpty
+    @JsonProperty("description")
+    private String description;
+
+    @NotEmpty
+    @JsonProperty("isFullTime")
+    private Boolean isFullTime;
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getProposedSalary() {
+        return proposedSalary;
+    }
+
+    public void setProposedSalary(Integer proposedSalary) {
+        this.proposedSalary = proposedSalary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getFullTime() {
+        return isFullTime;
+    }
+
+    public void setFullTime(Boolean fullTime) {
+        isFullTime = fullTime;
+    }
 }
