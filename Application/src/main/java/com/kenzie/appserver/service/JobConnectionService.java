9c30d540558fbeb81adf5bcc70a6d19ca152ca5b
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2023-01-12 03:49:39 -0700

    what I have so far for the client and the service, ignore the comments

--- Application/src/main/java/com/kenzie/appserver/service/JobConnectionService.java
+++ Application/src/main/java/com/kenzie/appserver/service/JobConnectionService.java
@@ -1,2 +1,49 @@
-package com.kenzie.appserver.service;public class JobConnectionService {
+package com.kenzie.appserver.service;
+
+import com.kenzie.appserver.service.model.ConnectionForUser;
+import com.kenzie.capstone.service.client.JobConnectionServiceClient;
+import com.kenzie.capstone.service.model.JobConnectionData;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobConnectionService {
+    ApplicationService applicationService;
+    JobPostService jobPostService;
+    JobConnectionServiceClient serviceClient;
+
+    @Autowired
+    public JobConnectionService(ApplicationService applicationService,
+                                JobPostService jobPostService,
+                                JobConnectionServiceClient serviceClient) {
+        this.applicationService = applicationService;
+        this.jobPostService = jobPostService;
+        this.serviceClient = serviceClient;
+    }
+
+    public List<ConnectionForUser> getAllJobsForUser(String username) {
+        List<ConnectionForUser> jobsForUser = new ArrayList<>();
+        List<JobConnectionData> dataFromLambda = serviceClient.getAllJobConnectionDataForUser(username);
+
+        for(JobConnectionData data : dataFromLambda) {
+            jobsForUser.add(new ConnectionForUser(data.getConnectionId(),
+                    data.getApplicationId(),
+                    data.getStatus(),
+                    jobPostService.getJobPost(data.getJobPostId())));
+        }
+
+        return jobsForUser;
+    }
+
+    public ConnectionForUser getJobForUser(String connectionId) {
+        JobConnectionData data = serviceClient.getJobConnectionData(connectionId);
+
+        ConnectionForUser jobForUser = new ConnectionForUser(data.getConnectionId(),
+                data.getApplicationId(),
+                data.getStatus(),
+                jobPostService.getJobPost(data.getJobPostId()));
+
+        return jobForUser;
+    }
 }
--- ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/JobConnectionServiceClient.java
+++ ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/JobConnectionServiceClient.java
@@ -1,4 +1,69 @@
 package com.kenzie.capstone.service.client;
 
-public class jobConnectionServiceClient {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kenzie.capstone.service.model.JobConnectionData;
+
+import java.util.List;
+
+public class JobConnectionServiceClient {
+
+    //private static final String GET_JOBCONNECTION_ENDPOINT = "jobconnection/{connectionId}";
+    //private static final String SET_JOBCONNECTION_ENDPOINT = "jobconnection";
+
+    private static final String GET_CONNECTION_ENDPOINT = "jobconnection/{connectionId}";
+    private static final String GET_ALL_USER_CONNECTIONS = "jobconnection/user/{username}";
+            //"user/{username}/jobconnection/all";
+
+    private ObjectMapper mapper;
+
+    public JobConnectionServiceClient() {
+        this.mapper = new ObjectMapper();
+    }
+
+    public JobConnectionData getJobConnectionData(String connectionId) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_CONNECTION_ENDPOINT)
+                .replace("{connectionId}", connectionId);
+        JobConnectionData connectionData;
+        try {
+            connectionData = mapper.readValue(response, JobConnectionData.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize Json: " + e);
+        }
+        return connectionData;
+    }
+
+    public List<JobConnectionData> getAllJobConnectionDataForUser(String username) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_ALL_USER_CONNECTIONS.replace("{username}", username));
+        List<JobConnectionData> dataForUser;
+        try {
+            dataForUser = mapper.readValue(response, new TypeReference<>(){});
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return dataForUser;
+    }
+
+
+
+
+
+    //todo check what is going on after this comment??
+
+
+//
+//    public JobConnectionData setJobConnectionData(String setData) {
+//        EndpointUtility endpointUtility = new EndpointUtility();
+//        String response = endpointUtility.postEndpoint(SET_JOBCONNECTION_ENDPOINT, setData);
+//        JobConnectionData connectionData;
+//        try {
+//            connectionData = mapper.readValue(response, JobConnectionData.class);
+//        }
+//        catch (Exception e) {
+//            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+//        }
+//        return connectionData;
+//    }
 }
