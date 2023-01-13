7b91ebde1c443d0392dde874465a101e7ca1b6f8
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2023-01-13 10:50:46 -0700

    fixing more merge conflicts

--- Application/src/main/java/com/kenzie/appserver/repositories/ApplicationRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ApplicationRepository.java
@@ -9,9 +9,5 @@ import java.util.List;
 
 @EnableScan
 public interface ApplicationRepository extends CrudRepository<ApplicationRecord, String> {
-<<<<<<< HEAD
-=======
-
->>>>>>> main
     List<ApplicationRecord> findByUsername(String username);
 }
